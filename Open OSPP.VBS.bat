a3ef236a30a6766523df8793c6ae8b4af600a9c7
YuukiConan <97886300+YuukiConan@users.noreply.github.com>
2022-02-10 21:48:53 +0700

    Add OSPP.VBS

--- Open OSPP.VBS.bat
+++ Open OSPP.VBS.bat
@@ -1,12 +1,14 @@
 @echo off
 
 echo ---------This Batch Created Using BatchFile Language----------
+echo Working with Office 2016 and Windows x64 System
 
-IF DEFINED (%ProgramFiles(x86)) (
+IF DEFINED (%ProgramFiles(x86)%) (
 goto loading
 
 :loading
 
+title = "######your name project" ######optional
 echo Please Wait...
 set TARGET_PATH= %ProgramFiles(x86)%
 goto openapp
@@ -15,22 +17,32 @@ goto openapp
 
 echo Open OSPP.VBS....
 start "%TARGET_PATH%\Microsoft Office\Office16\OSPP.VBS"
-goto load1
+goto load
+
+ :load
+
+     echo Copying OSPP.VBS....
+        set path= "%TARGET_PATH%\Microsoft Office\Office16"
+        set file= %TARGET_PATH%\Microsoft Office\Office16\OSPP.VBS"
+        copy= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
+        copy to= "C://Users/Admin/Dekstop/"
+ goto load1
 
 :load1
 
-       set path= "%TARGET_PATH%\Microsoft Office\Office16\"
-     set file= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
-   copy= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
-  copy to= "C://Users/Admin/Dekstop/"
+     echo Copying SLERROR.XML....
+       set path= "%TARGET_PATH%\Microsoft Office\Office16"
+       set file= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
+       copy= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
+       copy to= "C://Users/Admin/Dekstop/"
  goto load2
 
 :load2
 
- set file= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
-  set file= "%TARGET_PATH%\Microsoft Office\Office16\vNextDiag.ps1"
-   set to= "%TARGET_PATH%\Microsoft Office\Office16\OSPP.VBS"
-    goto end
+       set file= "%TARGET_PATH%\Microsoft Office\Office16\SLERROR.xml"
+       set file= "%TARGET_PATH%\Microsoft Office\Office16\OSPP.HTM"
+       set file= "%TARGET_PATH%\Microsoft Office\Office16\vNextDiag.ps1"
+    set to= "%TARGET_PATH%\Microsoft Office\Office16\OSPP.VBS"
 
 :end
 
